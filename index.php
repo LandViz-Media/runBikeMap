bb6dc30be8e51b3d4cd99b754e7c7ae64a52940b
Christopher J. Seeger <cjseeger@iastate.edu>
2019-08-16 15:54:47 -0500

    replaced index file with php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>runBikeMaps</title>
+
+    <style>
+
+    </style>
+
+
+    	<script src="https://code.jquery.com/jquery-2.2.4.js"></script>
+
+
+
+
+
+    <script>
+
+    </script>
+
+</head>
+
+<body>
+
+
+
+
+</body>
+
+</html>
